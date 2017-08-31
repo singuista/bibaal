7c006edfe809a05d2dcc8f0db4fd233de1b70a1a
singuista <tensai213@gmail.com>
2017-08-31 01:06:33 -0500

    clean: extracted meta data code

--- app/src/pages/homePage/HomePage.js
+++ app/src/pages/homePage/HomePage.js
@@ -5,91 +5,10 @@ import {Link}        						from 'react-router'
 import DocumentTitle 						from 'react-document-title'
 import Panel 								from '../../components/panel/Panel'
 import AboutGallery 						from '../../components/aboutSlider/AboutSlider'
+import slidesData 							from '../../metaData/aboutGalleryData'
 
 const propTypes = {}
 let root
-const sides = [
-	{
-		slide: {
-			classOverride: 'overrideSlideClass',
-			caption: 'Description',
-			fallbackImg: {
-				img: '/images/forex-chart.jpg',
-				srcSet: '/images/forex-chart.jpg 1000w',
-				alt: 'image alt'
-			},
-			srcSet: [
-				{
-					media: 'all and (orientation: landscape)',
-					src: 'https://images2.alphacoders.com/153/thumb-1920-153272.jpg'
-				},
-				{
-					media: 'all and (orientation: portrait)',
-					src: 'https://i.pinimg.com/736x/0c/1d/17/0c1d178d50f2020617b2608b6864074d--death-note-cosplay-death-note-anime.jpg'
-				}
-			]
-		},
-		thumbnail: {
-			classOverride: 'overrideSlideClass',
-			name: 'Name of Thumbnail',
-			fallbackImg: {
-				img: '/images/forex-chart.jpg',
-				srcSet: '/images/forex-chart.jpg 1000w',
-				alt: 'image alt'
-			},
-			srcSet: [
-				{
-					media: 'all and (orientation: landscape)',
-					src: 'http://dingo.care2.com/pictures/petition_images/petition/188/907194-1490205999-wide.jpg'
-				},
-				{
-					media: 'all and (orientation: portrait)',
-					src: 'https://images-na.ssl-images-amazon.com/images/I/517qS8BwPZL._AC_SL230_.jpg'
-				}
-			]
-		}
-	},
-	{
-		slide: {
-			classOverride: 'overrideSlideClass',
-			caption: 'Description',
-			fallbackImg: {
-				img: '/images/forex-chart.jpg',
-				srcSet: '/images/forex-chart.jpg 1000w',
-				alt: 'image alt'
-			},
-			srcSet: [
-				{
-					media: 'all and (orientation: landscape)',
-					src: 'https://images2.alphacoders.com/153/thumb-1920-153272.jpg'
-				},
-				{
-					media: 'all and (orientation: portrait)',
-					src: 'https://i.pinimg.com/736x/0c/1d/17/0c1d178d50f2020617b2608b6864074d--death-note-cosplay-death-note-anime.jpg'
-				}
-			]
-		},
-		thumbnail: {
-			classOverride: 'overrideSlideClass',
-			name: 'Name of Thumbnail',
-			fallbackImg: {
-				img: '/images/forex-chart.jpg',
-				srcSet: '/images/forex-chart.jpg 1000w',
-				alt: 'image alt'
-			},
-			srcSet: [
-				{
-					media: 'all and (orientation: landscape)',
-					src: 'http://dingo.care2.com/pictures/petition_images/petition/188/907194-1490205999-wide.jpg'
-				},
-				{
-					media: 'all and (orientation: portrait)',
-					src: 'https://images-na.ssl-images-amazon.com/images/I/517qS8BwPZL._AC_SL230_.jpg'
-				}
-			]
-		}
-	}
-]
 
 class HomePage extends React.Component {
 	/**
@@ -110,6 +29,7 @@ class HomePage extends React.Component {
 	**/
 	componentWillMount() {
 		//console.log('About to mount App')
+		console.log(slidesData)
 	}
 
 	/**
@@ -254,7 +174,7 @@ class HomePage extends React.Component {
 							<AboutGallery
 								id="BiabaalAbout"
 								class="aboutSliderOveride"
-								sliderInfo={sides}
+								sliderInfo={slidesData}
 							/>
 						</div>
 					</Panel>
